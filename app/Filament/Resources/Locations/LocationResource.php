57f12afe02a406ea52d48f85655786de61c13b1e
Rizky Dhani <rizkydhni03@gmail.com>
2026-05-20 16:54:13 +0700

    Move LocationResource under Rooms parent item

--- app/Filament/Resources/Locations/LocationResource.php
+++ app/Filament/Resources/Locations/LocationResource.php
@@ -20,7 +20,7 @@ class LocationResource extends Resource
 
     protected static string | UnitEnum | null $navigationGroup = 'System Management';
 
-    protected static ?string $navigationParentItem = 'Employees';
+    protected static ?string $navigationParentItem = 'Rooms';
 
     protected static string | BackedEnum | null $navigationIcon = 'heroicon-o-building-office';
 
